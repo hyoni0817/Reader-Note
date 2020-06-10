524c0dc39b2d0d6bff5a7b9392736c479296b841
hyoni0817 <dudu1104@naver.com>
2020-06-11 03:57:38 +0900

    Add: 로그인 폼

---
+++ front/components/LoginForm.js
@@ -0,0 +1,36 @@
+import React, { useCallback } from 'react';
+import Link from 'next/link';
+import { Input, Button, Form } from 'antd';
+import { useInput } from '../pages/signup';
+
+const LoginForm = () => {
+    const [id, onChangeId] = useInput('');
+    const [password, onChangePassword] = useInput('');
+    const onSubmitForm = useCallback((e) => {
+        e.preventDefault();
+        console.log({
+            id, password,
+        })
+    }, [id, password]);
+
+    return (
+        <Form onSubmit={onSubmitForm}>
+            <div>
+                <label htmlFor="user-id"></label>
+                <br />
+                <Input name="user-id" value={id} onChange={onChangeId} required />
+            </div>
+            <div>
+                <label htmlFor="user-password">비밀번호</label>
+                <br />
+                <Input name="user=password" value={password} onChange={onChangePassword} type="password" required />
+            </div>
+            <div>
+                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                <Link href="/signup"><a><Button>회원 가입</Button></a></Link>
+            </div>
+        </Form>
+    )
+}
+
+export default LoginForm;
