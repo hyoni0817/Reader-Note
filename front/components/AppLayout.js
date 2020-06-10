b92db901707669a0e8bb1ff9b0f85579e45a4204
hyoni0817 <dudu1104@naver.com>
2020-06-11 03:53:09 +0900

    Add: 로그인 전후 화면

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import LoginForm from './LoginForm';
 
-import { Menu, Input, Button } from 'antd';
+import { Menu, Input, Row, Col, Card, Avatar } from 'antd';
+
+const dummy = {
+    nickname: '피카츄',
+    Post: [],
+    Followings: [],
+    Followers: [],
+    isLoggedIn: false,
+};
 
 const AppLayout = ({children}) => {
     return (
@@ -14,8 +23,31 @@ const AppLayout = ({children}) => {
                     <Input.Search enterButton style={{verticalAlign: 'middle'}} />
                 </Menu.Item>
             </Menu>
-            <Link href="/signup"><a><Button>회원 가입</Button></a></Link>
-            {children}
+            <Row>
+                <Col xs={24} md={6}>
+                    { dummy.isLoggedIn ? <Card
+                        actions={[
+                            <div key="twit">쨱쨱<br />{dummy.Post.length}</div>,
+                            <div key="following">팔로잉<br />{dummy.Followings.length}</div>,
+                            <div key="follower">팔로워<br />{dummy.Followers.length}</div>
+                        ]}
+                    >
+                        <Card.Meta
+                            avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+                            title={dummy.nickname}
+                        />
+                    </Card>
+                        :
+                        <LoginForm />
+                    }
+                </Col> 
+                <Col xs={24} md={12}>
+                    {children}    
+                </Col> 
+                <Col xs={24} md={6}>
+                
+                </Col> 
+            </Row>
         </div>
     )
 }
