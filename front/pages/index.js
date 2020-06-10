b13995f47994b124eb010fe5b58c81c28a0219e2
hyoni0817 <dudu1104@naver.com>
2020-06-11 00:05:31 +0900

    Remove: 중복되는 Head 및 Layout

--- front/pages/index.js
+++ front/pages/index.js
@@ -3,19 +3,11 @@
 //그러나 {useState, useEffect} 같은 것을 사용해야 하기 때문에 아예 안 쓸 수는 없다.
 import React from 'react';
 //import Link from 'next/link';
-import Head from 'next/head';
-import AppLayout from '../components/AppLayout';
 
 const Home = () => {
     return (
         <>
-            <Head>
-                <title>Reader Note</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
-            </Head>
-            <AppLayout>
                 <div>Hello, Next!</div>
-            </AppLayout>
         </>
     )
 };
