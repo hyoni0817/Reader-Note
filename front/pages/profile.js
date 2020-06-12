8fd0047d4fccdb1edbf889515985ac75f06bdadf
hyoni0817 <dudu1104@naver.com>
2020-06-13 02:53:35 +0900

    Add; 닉네임 수정, 팔로잉 및 팔로워 목록

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,13 +1,43 @@
-//프로필 화면
+//프로필 화면(팔로워 및 팔로잉 목록, 나의 게시글 정보)
 import React from 'react';
+import { Form, Input, Button, List, Card, Icon } from 'antd';
 
 const Profile = () => {
     return (
-        <>
-            <div>
-                프로필
-            </div>
-        </> 
+        <div>
+            <Form style={{ marginBottom: '20px', border: '1px solid #d9d9d9', padding: '20px'}}>
+                <Input addonBefore="닉네임" />
+                <Button type="primary">수정</Button>
+            </Form>
+            <List 
+                style={{ marginBottom: '20px'}}
+                grid={{ gutter: 4, xs: 2, md: 3}}
+                size="small"
+                header={<div>팔로잉 목록</div>}
+                loadMore={<Button style={{ width: '100%' }}>더 보기</Button>}
+                bordered
+                dataSource={['Klein', '최지수', '황현정']}
+                renderItem={item => (
+                    <List.Item style={{ marginTop: '20px' }}>
+                        <Card actions={[<Icon key="stop" type="stop" />]}><Card.Meta description={item} /></Card>
+                    </List.Item>
+                )}
+            />
+            <List 
+                style={{ marginBottom: '20px'}}
+                grid={{ gutter: 4, xs: 2, md: 3}}
+                size="small"
+                header={<div>팔로워 목록</div>}
+                loadMore={<Button style={{ width: '100%' }}>더 보기</Button>}
+                bordered
+                dataSource={['klein', '최지수', '황현정']}
+                renderItem={item => (
+                    <List.Item style={{ marginTop: '20px' }}>
+                        <Card actions={[<Icon key="stop" type="stop" />]}><Card.Meta description={item} /></Card>
+                    </List.Item>
+                )}
+            />
+        </div> 
     );
 };
 
