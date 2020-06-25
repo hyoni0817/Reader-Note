5b98aa0a3389edb0c343dff58d06c4618064733b
hyoni0817 <dudu1104@naver.com>
2020-06-26 00:21:13 +0900

    Remove: ADD_POST & ADD_DUMMY

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -10,37 +10,8 @@ export const initialState = {
     imagePaths: [],
 };
 
-const ADD_POST = 'ADD_POST';
-const ADD_DUMMY = 'ADD_DUMMY';
-
-export const addPost = {
-    type: ADD_POST,
-};
-
-export const addDummy = {
-    type: ADD_DUMMY,
-    data: {
-        content: 'Hello',
-        UserId: 1,
-        User: {
-            nickname: '제로초',
-        }
-    }
-};
-
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST: {
-            return {
-                ...state, 
-            }
-        }
-        case ADD_DUMMY: {
-            return {
-                ...state, 
-                mainPosts: [action.data, ...state.mainPosts],
-            }
-        }
         default: {
             return {
                 ...state,
