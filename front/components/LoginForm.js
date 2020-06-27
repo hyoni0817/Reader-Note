a9e27bb077677e205958979083bf092991127963
hyoni0817 <dudu1104@naver.com>
2020-06-27 22:03:32 +0900

    Add: <Button>의 loading값에 isLoggingIn(로그인 시도 여부) 추가

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -1,17 +1,18 @@
 import React, { useCallback } from 'react';
 import Link from 'next/link';
 import { Input, Button, Form } from 'antd';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useInput } from '../pages/signup';
 import { LOG_IN_REQUEST } from '../reducers/user';
 const LoginForm = () => {
     const [id, onChangeId] = useInput('');
     const [password, onChangePassword] = useInput('');
+    const { isLoggingIn } = useSelector(state => state.user);
     const dispatch = useDispatch();
     
     const onSubmitForm = useCallback((e) => {
         e.preventDefault();
-
+        
          dispatch({
              type: LOG_IN_REQUEST,
              data: {
@@ -33,7 +34,7 @@ const LoginForm = () => {
                 <Input name="user=password" value={password} onChange={onChangePassword} type="password" required />
             </div>
             <div style={{ marginTop: '10px' }}>
-                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                <Button type="primary" htmlType="submit" loading={isLoggingIn}>로그인</Button>
                 <Link href="/signup"><a><Button>회원 가입</Button></a></Link>
             </div>
         </Form>
