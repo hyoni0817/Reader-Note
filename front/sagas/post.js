02d1dbcef11279f2a38784a5525855fde6b93a10
hyoni0817 <dudu1104@naver.com>
2020-06-29 02:05:28 +0900

    Add: 댓글 작성 관련 saga
    - 댓글 작성 후 작성 성공 여부에 대한 액션을 실행하기 위해 추가함.

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,6 +1,6 @@
 //post 리듀서만 담당
 import { all, fork, takeLatest, put, delay } from 'redux-saga/effects'
-import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE } from '../reducers/post';
+import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE, ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE } from '../reducers/post';
 
 function* addPostAPI() {
 
@@ -24,8 +24,36 @@ function* watchAddPost() {
 
 }
 
+function* addCommentAPI() {
+
+}
+function* addComment(action) { //이 액션은 PostCard.js의 ADD_COMMENT_REQUEST가 디스패치할 때 전달되는 액션이다. 
+    try {
+        yield delay(2000);
+        yield put({
+            type: ADD_COMMENT_SUCCESS,
+            data: {
+                postId: action.data.postId
+            }
+        });
+    } catch (e) {
+        console.log(e)
+        yield put({
+            type: ADD_COMMENT_FAILURE,
+            error: e,
+        })
+    }
+}
+
+function* watchAddComment() {
+    
+    yield takeLatest(ADD_COMMENT_REQUEST, addComment); //작성 버튼을 여러번 누르면 한번만 등록이 되야 하는게 맞기 때문에 takeLastest 사용
+
+}
+
 export default function* postSaga() {
     yield all([
         fork(watchAddPost),
+        fork(watchAddComment),
     ]);
 }
