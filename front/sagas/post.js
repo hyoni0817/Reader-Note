095c82d558d2c6f61ed17b2c8ee2986e3d18f076
hyoni0817 <dudu1104@naver.com>
2020-06-28 21:37:15 +0900

    Add: addPostAPI(게시글 추가 API)

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -2,6 +2,9 @@
 import { all, fork, takeLatest, put, delay } from 'redux-saga/effects'
 import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE } from '../reducers/post';
 
+function* addPostAPI() {
+
+}
 function* addPost() {
     try {
         yield delay(2000);
