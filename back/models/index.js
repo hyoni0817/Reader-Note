ef7a8d5cd043a7d0a5f201c04ced12e80e3963c2
hyoni0817 <dudu1104@naver.com>
2020-06-30 20:28:56 +0900

    Add:  sequelize 생성 후 모델 관련 js 파일을 불러와 DB객체에 모델을 정의

---
+++ back/models/index.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+//config.json에 설정한 것들을 적용(sequelize를 통해 db조작, 연결, 종료 -> sql문을 사용하지 않고 js를 통해 db를 컨트롤하는 방법)
+const sequelize = new Sequelize(config.database, config.username, config.password, config); 
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.Comment = require('./comment')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+db.Image = require('./image')(sequelize, Sequelize);
+db.Post = require('./post')(sequelize, Sequelize);
+db.User = require('./user')(sequelize, Sequelize);
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
