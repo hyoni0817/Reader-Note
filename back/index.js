762e75599c5925ccb1e18384a1dff78dbbdfa9e9
hyoni0817 <dudu1104@naver.com>
2020-07-07 20:43:58 +0900

    Add: 로그인 작업을 위해 쿠키, 세션, passport

--- back/index.js
+++ back/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
 
 const db = require('./models'); //index는 생략 가능함.
 const userAPIRouter = require('./routes/user');
 const postAPIRouter = require('./routes/post');
 const postsAPIRouter = require('./routes/posts');
 
+dotenv.config(); //.env 파일을 불러온다
 const app = express();
 db.sequelize.sync();//알아서 테이블을 생성해준다. 
 
@@ -16,6 +21,19 @@ app.use(morgan('dev')); //요청 들어오는 것에 대한 로그를 남기기 
 app.use(express.json()); //json 형식의 본문 처리
 app.use(express.urlencoded({ extended: true })); //form으로 넘어온 데이터 처리
 app.use(cors()); 
+app.use(cookieParser(process.env.COOKIE_SECRET)); 
+app.use(expressSession({ 
+    resave: false, 
+    saveUninitialized: false, 
+    
+    secret: process.env.COOKIE_SECRET, 
+    cookie: {
+        httpOnly: true, 
+        secure: true,
+    }
+}));
+app.use(passport.initialize());
+app.user(passport.session()); 
 
 //API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use('/api/user', userAPIRouter);
