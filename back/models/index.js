cbf9c6286c2aefa05f99c544c6fa69359da6f204
hyoni0817 <dudu1104@naver.com>
2020-07-13 01:05:18 +0900

    Change: db에 연결한 모델들위치를 위로 이동
    - db에 모델을 먼저 연결을 한 후 associate함수를 불러와야 하기 때문에 위치를 변경함.

--- back/models/index.js
+++ back/models/index.js
@@ -6,17 +6,18 @@ const db = {};
 //config.json에 설정한 것들을 적용(sequelize를 통해 db조작, 연결, 종료 -> sql문을 사용하지 않고 js를 통해 db를 컨트롤하는 방법)
 const sequelize = new Sequelize(config.database, config.username, config.password, config); 
 
+db.Comment = require('./comment')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+db.Image = require('./image')(sequelize, Sequelize);
+db.Post = require('./post')(sequelize, Sequelize);
+db.User = require('./user')(sequelize, Sequelize);
+
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
-db.Comment = require('./comment')(sequelize, Sequelize);
-db.Hashtag = require('./hashtag')(sequelize, Sequelize);
-db.Image = require('./image')(sequelize, Sequelize);
-db.Post = require('./post')(sequelize, Sequelize);
-db.User = require('./user')(sequelize, Sequelize);
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
