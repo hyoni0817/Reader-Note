9638637db732f4a06eaed115bdb07624ae6841cf
hyoni0817 <dudu1104@naver.com>
2020-07-22 23:02:37 +0900

    Add: 해시태그마다 다른 화면을 나타내기 위한 코드

--- front/pages/hashtag.js
+++ front/pages/hashtag.js
@@ -1,9 +1,35 @@
-import React from 'react';
-
-const Hashtag = () => {
+import React, { useEffect } from 'react';
+import propTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { LOAD_HASHTAG_POSTS_REQUEST } from '../reducers/post';
+import PostCard from '../components/PostCard';
+const Hashtag = ({ tag }) => {
+    console.log(tag);
+    const dispatch = useDispatch();
+    const { mainPosts } = useSelector(state => state.user);
+    useEffect(() => {
+        dispatch({
+            type: LOAD_HASHTAG_POSTS_REQUEST,
+            data: tag,
+        })
+    }, []);
     return (
-        <div>Hashtag</div>
+        <div>
+            {mainPosts.map( c => {
+                <PostCard key={+c.createdAt} post={c} />
+            })}
+        </div>
     );
+};
+
+Hashtag.propTypes = {
+    tag: propTypes.string.isRequired,
+}
+
+Hashtag.getInitialProps = async (context) => { 
+    
+    console.log('hashtag getInitialProps', context.query.tag);
+    return { tag: context.query.tag }; 
 };
 
 export default Hashtag;
