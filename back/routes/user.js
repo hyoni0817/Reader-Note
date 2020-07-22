320d9a363e62bf8f96940e86b5f8e67b68253952
hyoni0817 <dudu1104@naver.com>
2020-07-22 23:27:44 +0900

    Add: 다른 사용자의 정보를 가져오는 라우터

--- back/routes/user.js
+++ back/routes/user.js
@@ -40,8 +40,36 @@ router.post('/', async (req, res, next) => { // POST /api/user 회원가입
         return next(e); 
     }
 });
-router.get('/:id', (req, res) => { //남의 정보 가져오는 것 ex)/api/user/3(아이디가 3인 유저 정보를 가져오겠다는 의미)
-
+router.get('/:id', async (req, res, next) => { //남의 정보 가져오는 것 ex)/api/user/3(아이디가 3인 유저 정보를 가져오겠다는 의미), id 공간에 동적으로 변할 수 있는 것을 넣어준다. 
+    try {
+        await db.User.findOne({
+            where: { id: parseInt(req.params.id, 10)},
+            include: [{
+                model: db.Post,
+                as: 'Posts',
+                attributes: ['id'],
+            }, {
+                model: db.User,
+                as: 'Followings',
+                attributes: ['id'],
+            }, {
+                model: db.User,
+                as: 'Followers',
+                attributes: ['id'],
+            }],
+            attributes: ['id', 'nickname'],
+        });
+        
+        const jsonUser = user.toJson(); 
+        jsonUser.Posts = jsonUser.Posts ? jsonUser.Posts.length : 0;
+        jsonUser.Followings = jsonUser.Followings ? jsonUser.Followings.length : 0;
+        jsonUser.Followers = jsonUser.Followers ? jsonUser.Followers.length : 0;
+        jsonUser
+        req.json(user);
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }
 });
 router.post('/logout', (req, res) => {
     req.logout();
@@ -104,8 +132,23 @@ router.delete('/:id/follower', (req, res) => {
 });
 
 //app.get(...) 과 같은 이부분은 router이고 (req, res) => {...}은 controller라고 한다. 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', async (req, res) => {
+    try {
+        const posts = await db.Post.findAll({
+            where: {
+                UserId: parseInt(req.params.id, 10), //작성자는 UserId 컬럼에 있다. 
+                RetweetId: null, //리트윗한 게시글 빼고 내가쓴 게시글만 불러온다.
+            },
+            include: [{
+                model: db.User,
+                attributes: ['id', 'nickname'], //게시글 작성자를 불러올때는 항상 attributes로 가져온다(비밀번호를 제외시키고 가져와야하기 때문)
+            }],
+        });
+        res.json(posts);
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }
 });
 
 module.exports = router;
