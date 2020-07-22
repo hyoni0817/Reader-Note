1306c6742dfa015296a97eb0bfc69ee087482df0
hyoni0817 <dudu1104@naver.com>
2020-07-23 01:33:06 +0900

    Fix: 실수로 postsAPIRouter 삭제한 것을 다시 추가

--- back/index.js
+++ back/index.js
@@ -46,6 +46,7 @@ app.use(passport.session());
 //API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
+app.use('/api/posts', postsAPIRouter);
 app.use('/api/hashtag', hashtagAPIRouter);
 
 app.listen(3065, () => {
