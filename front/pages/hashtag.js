1387d7f44ec3f81bb002d0f341406410acaa5850
hyoni0817 <dudu1104@naver.com>
2020-07-23 21:17:34 +0900

    Fix: map메서드 내 소괄호로 고치고, PropTypes의 첫글자를 대문자로 고침
    - return문을 사용하지 않았기때문에 소괄호를 사용해야함.

--- front/pages/hashtag.js
+++ front/pages/hashtag.js
@@ -1,33 +1,34 @@
 import React, { useEffect } from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { LOAD_HASHTAG_POSTS_REQUEST } from '../reducers/post';
 import PostCard from '../components/PostCard';
+
 const Hashtag = ({ tag }) => {
     console.log(tag);
     const dispatch = useDispatch();
     const { mainPosts } = useSelector(state => state.post);
+        
     useEffect(() => {
         dispatch({
             type: LOAD_HASHTAG_POSTS_REQUEST,
             data: tag,
-        })
+        });
     }, []);
     return (
         <div>
-            {mainPosts.map( c => {
+            {mainPosts.map(c => (
                 <PostCard key={+c.createdAt} post={c} />
-            })}
+            ))}
         </div>
     );
 };
 
 Hashtag.propTypes = {
-    tag: propTypes.string.isRequired,
-}
+    tag: PropTypes.string.isRequired,
+};
 
 Hashtag.getInitialProps = async (context) => { 
-    
     console.log('hashtag getInitialProps', context.query.tag);
     return { tag: context.query.tag }; 
 };
