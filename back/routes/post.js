f791a05bebc069c39c26d91c4b5fa75ec2a66c29
hyoni0817 <dudu1104@naver.com>
2020-07-27 13:25:34 +0900

    Add: 댓글 보기 및 추가하는 라우터

--- back/routes/post.js
+++ back/routes/post.js
@@ -36,4 +36,61 @@ router.post('/images', (req, res) => {
 
 });
 
+router.get('/:id/comments', async (req, res, next) => { //게시글의 댓글 가져오기
+    try {
+        const post = await db.Post.findOne({ where: { id: req.params.id } });
+        if (!post) {
+            return res.status(404).send('포스트가 존재하지 않습니다.'); 
+        }
+        const comments = await db.Comment.findAll({
+            where: {
+                PostId: req.params.id,
+            },
+            order: [['createdAt', 'ASC']], 
+            include: [{
+                model: db.User,
+                attributes: ['id', 'nickname'],
+            }],
+        });
+        res.json(comments);
+    } catch (e) {
+        console.log(e);
+        next(e);
+    }
+});
+
+router.post('/:id/comment', async (req, res, next) => { //POST ex) /api/post/3/comment
+    try {
+        // 로그인한 사용자만 댓글 달기
+        if(!req.user) { 
+            res.status(401).send('로그인이 필요합니다.');
+        }
+
+        const post = await db.Post.findOne({ where: { id: req.params.id } });
+        if (!post) {
+            return res.status(404).send('포스트가 존재하지 않습니다.'); 
+        }
+
+        const newComment = await db.Comment.create({
+            PostId: post.id,
+            UserId: req.user.id,
+            content: req.body.content,
+        });
+        await post.addComment(newComment.id); 
+        const comment = await db.Comment.findOne({ 
+            where: {
+                id: newComment.id,
+            }, 
+            include: [{
+                model: db.User,
+                attribute: ['id', 'nickname'],
+            }], 
+        });
+        return res.json(comment);
+    } catch (e) {
+        console.error(e);
+        return next(e);
+    }
+});
+
 module.exports = router;
