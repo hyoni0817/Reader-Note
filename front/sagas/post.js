fbb21824f4c794d1cd95554c5f09d6f4e7202b97
hyoni0817 <dudu1104@naver.com>
2020-07-27 13:43:42 +0900

    Add: 댓글 추가 및 작성된 댓글들 로드하는 saga

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,6 +1,6 @@
 //post 리듀서만 담당
 import { all, fork, takeLatest, put, delay, call } from 'redux-saga/effects'
-import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE, ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE, LOAD_MAIN_POSTS_REQUEST, LOAD_MAIN_POSTS_SUCCESS, LOAD_MAIN_POSTS_FAILURE, LOAD_HASHTAG_POSTS_REQUEST, LOAD_HASHTAG_POSTS_SUCCESS, LOAD_HASHTAG_POSTS_FAILURE, LOAD_USER_POSTS_REQUEST, LOAD_USER_POSTS_SUCCESS, LOAD_USER_POSTS_FAILURE } from '../reducers/post';
+import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE, ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE, LOAD_MAIN_POSTS_REQUEST, LOAD_MAIN_POSTS_SUCCESS, LOAD_MAIN_POSTS_FAILURE, LOAD_HASHTAG_POSTS_REQUEST, LOAD_HASHTAG_POSTS_SUCCESS, LOAD_HASHTAG_POSTS_FAILURE, LOAD_USER_POSTS_REQUEST, LOAD_USER_POSTS_SUCCESS, LOAD_USER_POSTS_FAILURE, LOAD_COMMENTS_REQUEST, LOAD_COMMENTS_SUCCESS, LOAD_COMMENTS_FAILURE } from '../reducers/post';
 import axios from 'axios';
 
 function addPostAPI(postData) {
@@ -28,20 +28,27 @@ function* watchAddPost() {
 
 }
 
-function* addCommentAPI() {
-
+function addCommentAPI(data) {
+    // if(!data.content){
+    //     return console.error("데이터 확인 요망,data.content:", data.content);
+    // }
+    return axios.post(`/post/${data.postId}/comment`, {content: data.content}, {
+        withCredentials: true 
+    });
 }
+
 function* addComment(action) { //이 액션은 PostCard.js의 ADD_COMMENT_REQUEST가 디스패치할 때 전달되는 액션이다. 
     try {
-        yield delay(2000);
+        const result = yield call(addCommentAPI, action.data);
         yield put({
             type: ADD_COMMENT_SUCCESS,
             data: {
-                postId: action.data.postId
+                postId: action.data.postId,
+                comment: result.data,
             }
         });
     } catch (e) {
-        console.log(e)
+        console.error(e)
         yield put({
             type: ADD_COMMENT_FAILURE,
             error: e,
@@ -50,9 +57,34 @@ function* addComment(action) { //이 액션은 PostCard.js의 ADD_COMMENT_REQUES
 }
 
 function* watchAddComment() {
-    
-    yield takeLatest(ADD_COMMENT_REQUEST, addComment); //작성 버튼을 여러번 누르면 한번만 등록이 되야 하는게 맞기 때문에 takeLastest 사용
+    yield takeLatest(ADD_COMMENT_REQUEST, addComment); 
+}
 
+function loadCommentsAPI(postId) {
+    return axios.get(`/post/${postId}/comments`);
+}
+
+function* loadComments(action) { 
+    try {
+        const result = yield call(loadCommentsAPI, action.data);
+        yield put({
+            type: LOAD_COMMENTS_SUCCESS,
+            data: {
+                postId: action.data,
+                comments: result.data,
+            }
+        });
+    } catch (e) {
+        console.log(e)
+        yield put({
+            type: LOAD_COMMENTS_FAILURE,
+            error: e,
+        })
+    }
+}
+
+function* watchLoadComments() {
+    yield takeLatest(LOAD_COMMENTS_REQUEST, loadComments); 
 }
 
 function loadMainPostsAPI() {
@@ -134,6 +166,7 @@ export default function* postSaga() {
         fork(watchLoadMainPosts),
         fork(watchAddPost),
         fork(watchAddComment),
+        fork(watchLoadComments),
         fork(watchLoadHashtagPosts),
         fork(watchLoadUserPosts),
     ]);
