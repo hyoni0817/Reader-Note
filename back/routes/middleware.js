53526e64da350fdc95d103606eab6881c94e5480
hyoni0817 <dudu1104@naver.com>
2020-07-29 00:01:12 +0900

    Correct: isAuthenticated 오타 수정

--- back/routes/middleware.js
+++ back/routes/middleware.js
@@ -1,6 +1,6 @@
 //로그인 여부 체크
 exports.isLoggedIn = (req, res, next) => {
-    if(req.isAuthenticate()) {
+    if(req.isAuthenticated()) {
         next(); 
     } else {
         res.status(401).send('로그인이 필요합니다.'); 
@@ -9,7 +9,7 @@ exports.isLoggedIn = (req, res, next) => {
 
 //로그인 안한 사용자 체크(회원가입 및 로그인한 페이지에 이미 로그인한 사용자 접근 불가)
 exports.isNotLoggedIn = (req, res, next) => {
-    if(!req.isAuthenticate()) {
+    if(!req.isAuthenticated()) {
         next(); 
     } else {
         res.status(401).send('로그인이 필요합니다.'); 
