90287839f17981214488ca88ce10ff47ffd07097
hyoni0817 <dudu1104@naver.com>
2020-08-16 23:49:49 +0900

    Add: 게시글 작성자 및 게시글 좋아요 누른 사람 목록 include

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -8,9 +8,14 @@ router.get('/', async (req, res, next) => { //GET /api/posts
        const posts = await db.Post.findAll({
             include: [{
                 model: db.User,
-                attributes: ['id', 'nickname'], 
+                attributes: ['id', 'nickname'], //게시글 작성자 include
             }, {
                 model: db.Image,
+            }, {
+                model: db.User, //게시글 좋아요 누른 사람들 목록 include 
+                through: 'Like',
+                as: 'Likers', 
+                attributes: ['id'],
             }],
             order: [['createdAt', 'DESC']], 
        })
