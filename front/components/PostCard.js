c4ffe12069a0f7c90f3610487be63b73bd5450c6
hyoni0817 <dudu1104@naver.com>
2020-08-22 01:00:07 +0900

    Fix: 이미지 속성 존재 여부를 먼저 체크하지 않아 발생한 오류 해결

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -97,7 +97,7 @@ const PostCard = ({ post }) => {
         <div>
         <Card
             key={+post.createdAt}
-            cover={post.Images[0] && <PostImages images={post.Images} />}
+            cover={post.Images && post.Images[0] && <PostImages images={post.Images} />}
             actions={[
                 <Icon type="retweet" key="retweet" onClick={onRetweet}/>,
                 <Icon type="heart" key="heart" theme={ liked ? 'twoTone' : 'outlined'} twoToneColor="#eb2f96" onClick={onToggleLike} />,
