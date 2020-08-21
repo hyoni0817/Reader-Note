98b381b28916772c66e17b18b77e2ea455073e6e
hyoni0817 <dudu1104@naver.com>
2020-08-21 23:53:16 +0900

    Add: 팔로잉 및 팔로워 목록 가져오기, 팔로워 제거 관련 saga

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,7 +1,7 @@
 //user 리듀서만 담당
 import { all, takeLatest, put, call, fork, take, takeEvery, delay } from 'redux-saga/effects'
 import axios from 'axios'; //서버에 요청을 보내주는 모듈
-import { LOG_IN_SUCCESS, LOG_IN_FAILURE, LOG_IN_REQUEST, SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE, LOG_OUT_SUCCESS, LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOAD_USER_SUCCESS, LOAD_USER_FAILURE, LOAD_USER_REQUEST, FOLLOW_USER_SUCCESS, FOLLOW_USER_FAILURE, FOLLOW_USER_REQUEST, UNFOLLOW_USER_SUCCESS, UNFOLLOW_USER_FAILURE, UNFOLLOW_USER_REQUEST, } from '../reducers/user';
+import { LOG_IN_SUCCESS, LOG_IN_FAILURE, LOG_IN_REQUEST, SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE, LOG_OUT_SUCCESS, LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOAD_USER_SUCCESS, LOAD_USER_FAILURE, LOAD_USER_REQUEST, FOLLOW_USER_SUCCESS, FOLLOW_USER_FAILURE, FOLLOW_USER_REQUEST, UNFOLLOW_USER_SUCCESS, UNFOLLOW_USER_FAILURE, UNFOLLOW_USER_REQUEST, LOAD_FOLLOWERS_SUCCESS, LOAD_FOLLOWERS_FAILURE, LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWINGS_SUCCESS, LOAD_FOLLOWINGS_FAILURE, LOAD_FOLLOWINGS_REQUEST, REMOVE_FOLLOWER_SUCCESS, REMOVE_FOLLOWER_FAILURE, REMOVE_FOLLOWER_REQUEST, } from '../reducers/user';
 
 function logInAPI(loginData) {
     //서버에 요청을 보내는 부분
@@ -158,6 +158,84 @@ function* watchUnfollow() {
     yield takeEvery(UNFOLLOW_USER_REQUEST, unfollow); 
 }
 
+function loadFollowersAPI(userId) { 
+    return axios.get(`/user/${userId}/followers`, {
+        withCredentials: true, 
+    }); 
+}
+
+function* loadFollowers(action) { 
+    try {
+        const result = yield call(loadFollowersAPI, action.data); 
+        yield put({ 
+            type: LOAD_FOLLOWERS_SUCCESS,
+            data: result.data,
+        })
+    } catch (e) { 
+        console.error(e);
+        yield put({
+            type: LOAD_FOLLOWERS_FAILURE,
+            error: e,
+        })
+    }
+}
+
+function* watchLoadFollowers() {
+    yield takeEvery(LOAD_FOLLOWERS_REQUEST, loadFollowers); 
+}
+
+function loadFollowingsAPI(userId) { 
+    return axios.get(`/user/${userId}/followings`, {
+        withCredentials: true, 
+    }); 
+}
+
+function* loadFollowings(action) { 
+    try {
+        const result = yield call(loadFollowingsAPI, action.data); 
+        yield put({ 
+            type: LOAD_FOLLOWINGS_SUCCESS,
+            data: result.data,
+        })
+    } catch (e) { 
+        console.error(e);
+        yield put({
+            type: LOAD_FOLLOWINGS_FAILURE,
+            error: e,
+        })
+    }
+}
+
+function* watchLoadFollowings() {
+    yield takeEvery(LOAD_FOLLOWINGS_REQUEST, loadFollowings); 
+}
+
+function removeFollowersAPI(userId) { 
+    return axios.delete(`/user/${userId}/follower`, {
+        withCredentials: true, 
+    }); 
+}
+
+function* removeFollowers(action) { 
+    try {
+        const result = yield call(removeFollowersAPI, action.data); 
+        yield put({ 
+            type: REMOVE_FOLLOWER_SUCCESS,
+            data: result.data,
+        })
+    } catch (e) { 
+        console.error(e);
+        yield put({
+            type: REMOVE_FOLLOWER_FAILURE,
+            error: e,
+        })
+    }
+}
+
+function* watchRemoveFollower() {
+    yield takeEvery(REMOVE_FOLLOWER_REQUEST, removeFollowers); 
+}
+
 export default function* userSaga() {
     yield all([
         fork(watchLogIn),
@@ -166,5 +244,8 @@ export default function* userSaga() {
         fork(watchSignUp),
         fork(watchFollow),
         fork(watchUnfollow),
+        fork(watchLoadFollowers),
+        fork(watchLoadFollowings),
+        fork(watchRemoveFollower),
     ])
 }
