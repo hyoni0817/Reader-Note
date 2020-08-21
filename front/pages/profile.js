b5509c08fedbfe68a787f38e3b2286661f9eb582
hyoni0817 <dudu1104@naver.com>
2020-08-21 23:57:46 +0900

    Add: 프로필 페이지 내 기능 구현
    - 팔로잉 및 팔로워 목록 보기, 팔로워 제거 및 언팔로우, 본인이 작성한 글 보기 기능 추가

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,9 +1,47 @@
 //프로필 화면(팔로워 및 팔로잉 목록, 나의 게시글 정보)
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { Button, List, Card, Icon } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import PostCard from '../components/PostCard';
 import NicknameEditForm from '../components/NicknameEditForm';
+import { LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWINGS_REQUEST, UNFOLLOW_USER_REQUEST, REMOVE_FOLLOWER_REQUEST, } from '../reducers/user';
+import { LOAD_USER_POSTS_REQUEST } from '../reducers/post';
 
 const Profile = () => {
+    const dispatch = useDispatch();
+    const { me, followingList, followerList } = useSelector(state => state.user);
+    const { mainPosts } = useSelector(state => state.post);
+
+    useEffect(() => {
+        if (me) {
+            dispatch({
+                type: LOAD_FOLLOWERS_REQUEST,
+                data: me.id,
+            });
+            dispatch({
+                type: LOAD_FOLLOWINGS_REQUEST,
+                data: me.id,
+            });
+            dispatch({
+                type: LOAD_USER_POSTS_REQUEST,
+                data: me.id,
+            });
+        }      
+    }, [ me && me.id ]);
+    
+    const onUnfollow = useCallback( userId => () => {
+        dispatch({
+            type: UNFOLLOW_USER_REQUEST,
+            data: userId,
+        })
+    }, []);
+
+    const onRemoveFollower = useCallback( userId => () => {
+        dispatch({
+            type: REMOVE_FOLLOWER_REQUEST,
+            data: userId,
+        })
+    }, []);
     return (
         <div>
             <NicknameEditForm />
@@ -14,10 +52,12 @@ const Profile = () => {
                 header={<div>팔로잉 목록</div>}
                 loadMore={<Button style={{ width: '100%' }}>더 보기</Button>}
                 bordered
-                dataSource={['Klein', '최지수', '황현정']}
+                dataSource={ followingList }
                 renderItem={item => (
                     <List.Item style={{ marginTop: '20px' }}>
-                        <Card actions={[<Icon key="stop" type="stop" />]}><Card.Meta description={item} /></Card>
+                        <Card actions={[<Icon key="stop" type="stop" onClick={onUnfollow(item.id)} />]}>
+                            <Card.Meta description={item.nickname} />
+                        </Card>
                     </List.Item>
                 )}
             />
@@ -28,13 +68,20 @@ const Profile = () => {
                 header={<div>팔로워 목록</div>}
                 loadMore={<Button style={{ width: '100%' }}>더 보기</Button>}
                 bordered
-                dataSource={['klein', '최지수', '황현정']}
+                dataSource={ followerList }
                 renderItem={item => (
                     <List.Item style={{ marginTop: '20px' }}>
-                        <Card actions={[<Icon key="stop" type="stop" />]}><Card.Meta description={item} /></Card>
+                        <Card actions={[<Icon key="stop" type="stop" onClick={onRemoveFollower(item.id)} />]}>
+                            <Card.Meta description={item.nickname} />
+                        </Card>
                     </List.Item>
                 )}
             />
+            <div>
+            {mainPosts.map(c => (
+                    <PostCard key={+c.createdAt} post={c} />
+                ))}
+            </div>
         </div> 
     );
 };
