19960e61603f090567c1ec436ec7942d1ee20577
hyoni0817 <dudu1104@naver.com>
2020-08-30 23:47:42 +0900

    Add: 로그 확인을 위해 커스텀 미들웨어 추가

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -61,7 +61,10 @@ ReaderNote.getInitialProps = async (context) => {
 
 const configureStore = (initialState, options) => {
     const sagaMiddlewares = createSagaMiddleware(); //saga 미들웨어를 리덕스에 연결하기
-    const middlewares = [sagaMiddlewares]; // createSagaMiddleware()를 넣어주면 알아서 미들웨어 장착이 된다.
+    const middlewares = [sagaMiddlewares, (store) => (next) => (action) => { //custom 미들웨어이다. (커스텀 미들웨어는 삼단 curring 함수이다.)
+        console.log(action); //성공적인 액션은 console.log 한 게 찍힘.
+        next(action);
+    }]; // createSagaMiddleware()를 넣어주면 알아서 미들웨어 장착이 된다.
     const enhancer = process.env.NODE_ENV === 'production' ? compose(applyMiddleware(...middlewares)) : compose(applyMiddleware(...middlewares), !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ?
         window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
     ); // 배포할 때는 __REDUX_DEVTOOLS_EXTENSION__를 빼준다. 그렇지 않으면 redux 데이터가 노출되는데 redux가 state의 중앙 통제실이기 때문에 그것의 흐름이 노출되면 보안에 위협이 되기 때문에 빼줘야 한다.
