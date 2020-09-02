88d83064d1989bd3488651269b36999f11e6e925
hyoni0817 <dudu1104@naver.com>
2020-09-02 20:47:44 +0900

    Add: 수정, 삭제, 신고 버튼 생성 및 삭제 버튼 기능 구현

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,9 +1,9 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { Card, Icon, Button, Avatar, List, Form, Input, Comment} from 'antd';
+import { Card, Icon, Button, Avatar, List, Form, Input, Comment, Popover} from 'antd';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux'; 
-import { ADD_COMMENT_REQUEST, LOAD_COMMENTS_REQUEST, UNLIKE_POST_REQUEST, LIKE_POST_REQUEST, RETWEET_REQUEST, } from '../reducers/post';
+import { ADD_COMMENT_REQUEST, LOAD_COMMENTS_REQUEST, UNLIKE_POST_REQUEST, LIKE_POST_REQUEST, RETWEET_REQUEST, REMOVE_POST_REQUEST} from '../reducers/post';
 import { FOLLOW_USER_REQUEST, UNFOLLOW_USER_REQUEST } from '../reducers/user';
 import PostImages from './PostImages';
 import PostCardContent from './PostCardContent';
@@ -93,6 +93,13 @@ const PostCard = ({ post }) => {
             data: userId,
         })
     }, []);
+
+    const onRemovePost = useCallback(userId => () => {
+        dispatch({
+            type: REMOVE_POST_REQUEST,
+            data: userId,
+        })
+    })
     return (
         <div>
         <Card
@@ -102,7 +109,23 @@ const PostCard = ({ post }) => {
                 <Icon type="retweet" key="retweet" onClick={onRetweet}/>,
                 <Icon type="heart" key="heart" theme={ liked ? 'twoTone' : 'outlined'} twoToneColor="#eb2f96" onClick={onToggleLike} />,
                 <Icon type="message" key="message" onClick={onToggleComment} />,
-                <Icon type="ellipsis" key="ellipsis" />,
+                <Popover
+                    key="ellipsis"
+                    content={(
+                        <Button.Group>
+                            {me && post.UserId === me.id
+                            ? (
+                                <>
+                                    <Button>수정</Button>
+                                    <Button type="danger" onClick={onRemovePost(post.id)}>삭제</Button>
+                                </>
+                            )
+                            : <Button>신고</Button>}
+                        </Button.Group>
+                    )}
+                >
+                    <Icon type="ellipsis" key="ellipsis" />
+                </Popover>,
             ]}
             title={post.RetweetId ? `${post.User.nickname}님이 리트윗하셨습니다.` : null}
             extra={
