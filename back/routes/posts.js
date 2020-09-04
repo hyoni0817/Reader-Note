9cf5cac1234353905ad71bd5f4411a436c0edc37
hyoni0817 <dudu1104@naver.com>
2020-09-04 16:30:54 +0900

    Add: 인피니트 스크롤링과 관련한 where문(조건문)

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -5,7 +5,17 @@ const db = require('../models');
 
 router.get('/', async (req, res, next) => { //GET /api/posts 
     try {   
+        let where = {};
+        if (parseInt(req.query.lastId, 10)) { //lastId가 있는 경우
+            where = { //lastId보다 작은 값을 가진 id들을 가져와야함.
+                id: {
+                    [db.Sequelize.Op.lt]: parseInt(req.query.lastId, 10), //lt는 less than 이라는 뜻. 해당 코드줄의 뜻은 parseInt(req.query.lastId, 10) 보다 작은 것을 갖고 온다는 것. 
+                }
+            }
+        } 
+        // lastId가 0인 경우 처음부터 갖고 와야하기 때문에 조건문이 필요 없다. 
        const posts = await db.Post.findAll({
+            where,
             include: [{
                 model: db.User,
                 attributes: ['id', 'nickname'], //게시글 작성자 include
@@ -27,6 +37,7 @@ router.get('/', async (req, res, next) => { //GET /api/posts
                 }]
             }],
             order: [['createdAt', 'DESC']], 
+            limit: parseInt(req.query.limit, 10),
        })
        res.json(posts);
     } catch (e) {
