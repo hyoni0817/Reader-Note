611d5c6242bc6f7b123a06fe92bc7e14918923b3
hyoni0817 <dudu1104@naver.com>
2020-09-15 22:53:11 +0900

    Add: '/post/:id'의 동적 요청 처리를 위한 라우터

--- front/server.js
+++ front/server.js
@@ -30,6 +30,10 @@ app.prepare().then(() => {
             secure: false,
         },
     }));
+    
+    server.get('/post/:id', (req, res) => {
+        return app.render(req, res, '/post', { id: req.params.id });
+    });
 
     server.get('/hashtag/:tag', (req, res) => {
         return app.render(req, res, '/hashtag', { tag: req.params.tag }); 
