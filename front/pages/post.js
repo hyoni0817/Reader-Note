314e06f4b7be53b5505a4cc963a6d62a3c9af51d
hyoni0817 <dudu1104@naver.com>
2020-09-16 02:34:59 +0900

    Add: react-helmet을 사용하여 메타태그 추가
    - 검색엔진봇이 어떤 컨텐츠인지 구별할 수 있도록 메타태그를 추가해줌.

--- front/pages/post.js
+++ front/pages/post.js
@@ -2,12 +2,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 import { LOAD_POST_REQUEST } from '../reducers/post';
 
 const Post =  ({ id }) => {
     const { singlePost } = useSelector(state => state.post);
     return (
         <>
+            <Helmet 
+                title={`${singlePost.User.nickname}님의 글`}//헤드 태그의 title
+                description={singlePost.content}//게시글 내용이 descripttion
+                meta={[{
+                    name: 'description', content: singlePost.content,
+                }, {
+                    property: 'og:title', content: `${singlePost.User.nickname}님의 게시글`
+                }, {
+                    property: 'og:description', content: singlePost.content,
+                }, {
+                    property: 'og:image', content: singlePost.Images[0] && `http://localhost:3065/${singlePost.Images[0].src}`, 
+                }, {
+                    property: 'og:url', content: `http://localhost:3065/post/${id}`,
+                }]}
+                //메타태그는 여러 개 넣어줄 수 있기 때문에 배열을 사용해서 넣어줌 
+                //검색엔진 봇은 어떤게 어떤건지 구별을 잘 못하기 때문에 이렇게 일일이 다 구별해줘야 한다.
+            />
             <div>{singlePost.content}</div>
             <div>{singlePost.User.nickname}</div>
             <div>{singlePost.Images[0] && <img src={`http://localhost:3065/${singlePost.Images[0].src}`}/>}</div>
