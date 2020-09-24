c2b5e0c091a45315fd36b9bc6e34946fa5a30de9
hyoni0817 <dudu1104@naver.com>
2020-09-24 23:04:49 +0900

    Add: 날짜 라이브러리인 moment - tree shaking 예제를 위해 사용

--- front/containers/PostCard.js
+++ front/containers/PostCard.js
@@ -8,6 +8,8 @@ import { FOLLOW_USER_REQUEST, UNFOLLOW_USER_REQUEST } from '../reducers/user';
 import PostImages from '../components/PostImages';
 import PostCardContent from '../components/PostCardContent';
 import styled from 'styled-components';
+import moment from 'moment';
+moment.locale('ko'); //한글을 사용하겠다는 의미.moment는 다국어를 지원함.
 
 const CardWrapper = styled.div`
     margin-bottom: 20px;
@@ -167,6 +169,7 @@ const PostCard = ({ post }) => {
                     description={<PostCardContent postData={post.content} />}
                 />  
             )}
+            moment(post.createdAt).format('YYYY.MM.DD');
         </Card>
             {commentFormOpened && (
                 <>
