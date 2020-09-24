e254d91d5ef1b22228d5c780fa652a0545c1a734
hyoni0817 <dudu1104@naver.com>
2020-09-24 23:06:18 +0900

    Add: 번들 파일 용량 압축을 위한 코드

--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,6 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = withBundleAnalyzer({
     //여기에 설정들을 다 넣을 수 있다.
@@ -20,12 +22,33 @@ module.exports = withBundleAnalyzer({
         console.log('rules', config.module.rules[0]);
         
         const prod = process.env.NODE_ENV === 'production';
+        const plugins = [
+            ...config.plugins,
+            new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/), //moment github에서 이 코드 사용방법 알려줌. 이걸 사용하면 tree shakin이 됨.
+        ];
+        if (prod) {
+            plugins.push(new CompressionPlugin()); //배포일 때만 사용, 파일 확장자를 .gz(gZip)로 만들고(ex: main.js.gz) 용량을 3분의 1로 압축시켜줌
+        }
 
         return {
             ...config,
             mode: prod ? 'production' : 'development',
             //배포 환경인지 개발환경인지 구분해주기(배포 환경일 때는 hidden-source-map)
             devtool: prod ? 'hidden-source-map' : 'eval',
+            module: {
+                ...config.module,
+                rules: [
+                    ...config.module.rules,
+                    {
+                        loader: 'webpack-ant-icon-loader',
+                        enforce: 'pre',
+                        include: [
+                            require.resolve('@ant-design/icons/lib/dist'),
+                        ]
+                    }
+                ]
+            },     
+            plugins,       
         };//return으로 웹팩 설정 객체를 리턴해줘야 함.  그래서 기존 설정을 먼저 해준 다음에, 기존 설정을 덮어씌울 부분을 ...config 아래에 적어준다.
     }
 })
